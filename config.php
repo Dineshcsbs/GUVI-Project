45d507c827d4bbdda311327d11d200d948abd3da
Dineshcsbs <113577186+Dineshcsbs@users.noreply.github.com>
2023-11-26 10:46:44 +0530

    config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$mysql_host = 'localhost:3306';
+$mysql_user = 'root';
+$mysql_password = '123456';
+$mysql_database = 'user_login';
+
+
+
+$redis_host = 'your_redis_host';
+$redis_port = 'your_redis_port';
+?>
